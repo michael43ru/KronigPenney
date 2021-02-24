592f54ca821472dfc803bb842dfcfd196098f34f
michael43ru <55547179+michael43ru@users.noreply.github.com>
2021-02-25 01:25:20 +0300

    Add files via upload
    k(E)

--- F_example.m
+++ F_example.m
@@ -1,9 +1,20 @@
 U0 = -4;
-E = [U0:0.00001:25];
+E_0 = [U0:0.0001:25];
 a = 5 * 10^(-10);
 b = 2 * 10^(-9);
 m = 9.1094*10^(-28);
-f = F(E, m, a, b, U0);
-figure; hold on; grid on; plot(E, f);
-k = acos(f)/(a+b);
-figure; hold on; grid on; plot(E, k);
+f = F(E_0, m, a, b, U0);
+figure; hold on; grid on; plot(E_0, f);
+k_0 = acos(f)/(a+b);
+len = length(k_0);
+k = 0;
+E = 0;
+for i = 1:len
+    k(i) = -k_0(len-i+1);
+    E(i) = E_0(len-i+1);
+end
+for i = 1:len
+    k(i+len) = k_0(i);
+    E(i+len) = E_0(i);
+end
+figure; hold on; grid on; plot(k, E);
